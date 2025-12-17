fcc16675f84ba998b77c639702d5ca559316026f
Huỳnh Minh Trí <dh52201616@student.stu.edu.vn>
2025-12-17 10:10:54 +0700

    OSS-final

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome
+          HuynhMinhTri-Thu 4 ca 2
         </p>
         <a
           className="App-link"
